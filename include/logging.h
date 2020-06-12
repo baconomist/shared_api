47edb1ec1744885686ccab5956b416eec8c5e952
Lucas Borowiecki <baconomist@gmail.com>
2020-06-11 22:07:34 -0400

    std::exception logging

--- include/logging.h
+++ include/logging.h
@@ -6,10 +6,11 @@
 
 #include <string>
 
-namespace logging
+namespace shared_api::logging
 {
     void info(const std::string& message);
     void debug(const std::string& message);
     void warn(const std::string& message);
     void error(const std::string& message);
+    void exception(const std::exception &e);
 }
